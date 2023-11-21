f6906154e5b57645041f34605e2ec0591d811322
Akinjide Akinyanmi <mrakinyanmi@gmail.com>
2023-11-21 14:05:50 +0100

    linked local and remote

---
+++ src/main/java/mscreative/example/dlvr/DlvrApplication.java
@@ -0,0 +1,15 @@
+package mscreative.example.dlvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlvrApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DlvrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mscreative/example/dlvr/DlvrApplicationTests.java
@@ -0,0 +1,13 @@
+package mscreative.example.dlvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
