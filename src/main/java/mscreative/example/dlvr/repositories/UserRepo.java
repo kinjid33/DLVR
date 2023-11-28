86eccc10c62b45bc3b98ab634bc6649f16f4559b
Akinjide Akinyanmi <mrakinyanmi@gmail.com>
2023-11-28 15:54:46 +0100

    completed security configuration

---
+++ src/main/java/mscreative/example/dlvr/repositories/UserRepo.java
@@ -0,0 +1,10 @@
+package mscreative.example.dlvr.repositories;
+
+import mscreative.example.dlvr.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByEmail (String email);
+}
